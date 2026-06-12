24683120f1ee9d57e42ef2cd1ce704afbd19b50c
papis301 <pfaye3@gmail.com>
2026-06-12 23:18:35 +0000

    integration des couleurs ok

--- app/src/main/java/com/pisco/stockmanager/ui/theme/Color.kt
+++ app/src/main/java/com/pisco/stockmanager/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.pisco.stockmanager.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+//val Purple80 = Color(0xFFD0BCFF)
+//val PurpleGrey80 = Color(0xFFCCC2DC)
+//val Pink80 = Color(0xFFEFB8C8)
+//
+//val Purple40 = Color(0xFF6650a4)
+//val PurpleGrey40 = Color(0xFF625b71)
+//val Pink40 = Color(0xFF7D5260)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val BluePrimary = Color(0xFF1565C0)
+val BlueSecondary = Color(0xFF42A5F5)
+val BlueAccent = Color(0xFF90CAF9)
+
+val SuccessGreen = Color(0xFF2E7D32)
+val DangerRed = Color(0xFFD32F2F)
+
+val BackgroundLight = Color(0xFFF5F7FA)
+val SurfaceLight = Color(0xFFFFFFFF)
+
+val TextDark = Color(0xFF1E293B)
